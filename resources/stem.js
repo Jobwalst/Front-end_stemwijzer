0c55f2dc05539949457e05ac1a3c60798f281256
Job <99058203@mydavinci.nl>
2020-12-03 11:00:13 +0100

    question cycle
    questions can now be answered with: "eens", "geen van beide", or "oneens".
    
    all answers are saved in an array.
    
    after an answer, the next question appears.

--- resources/stem.js
+++ resources/stem.js
@@ -1,14 +1,14 @@
 var back = document.getElementsByClassName("backButton");
-var title = document.getElementsByClassName("questionTitle");
-var text = document.getElementsByClassName("questionText");
+var title = document.getElementsByClassName("questionTitle")[0];
+var text = document.getElementsByClassName("questionText")[0];
 
-var button1 = document.getElementsByClassName("button1");
-var button2 = document.getElementsByClassName("button2");
-var button3 = document.getElementsByClassName("button3");
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
 
 var skip = document.getElementsByClassName("skip");
 
-var question = 1;
+var question = 0;
 
 var questTitles = ['1. Bindend referendum', '2. Maatschappelijke dienstplicht', '3. Anoniem solliciteren', '4. Groepsbelediging', '5. Teelt en verkoop wiet', '6. Vervroegde vrijlating', '7. Vennootschapsbelasting', '8. Belasting hoogste inkomens', '9. Tijdelijke arbeidscontracten', '10. AOW-leeftijd 65', '11. Verzekering zzp`ers', '12. Leenstelsel studenten', '13. Geld cultuur', '14. Islamitische immigranten', '15. Kinderpardon', '16. Onderdak illegalen', '17. Hypotheekrente', '18. Verhuurdersheffing', '19. Schiphol', '20. Kilometerheffing', '21. Nieuwe wegen', '22. Kolencentrales', '23. BTW-tarief vlees', '24. Voltooit leven', '25. Afschaffing eigen risico', '26. Landelijk zorgfonds', '27. Defensie uitgaven', '28. Europees leger', '29. Ontwikkelingshulp', '30. EU-lidmaatschap'];//The title of each question is saved in this array.
 
@@ -18,3 +18,36 @@ var questDesc = ['Er moet een bindend referendum komen, waarmee burgers door het
 
 console.log(questDesc.length);
 
+var answers = [];//Answers can only be: "EENS", "GEEN VAN BEIDE" or "ONEENS".
+
+function showQuestion(){//This function shows a specific question based on the number in the variable: "question".
+    console.log(questTitles[question]);
+    console.log(questDesc[question]);
+    title.innerHTML = questTitles[question];
+    text.innerHTML = questDesc[question];
+    button1.setAttribute("onclick", "agree();");
+    button2.setAttribute("onclick", "none();");
+    button3.setAttribute("onclick", "disagree();");
+}
+showQuestion();
+
+function agree(){
+    answers.push('eens');
+    question++;
+    showQuestion();
+    console.log(answers);
+}
+
+function none(){
+    answers.push('geen van beide');
+    question++;
+    showQuestion();
+    console.log(answers);
+}
+
+function disagree(){
+    answers.push('oneens');
+    question++;
+    showQuestion();
+    console.log(answers);
+}
